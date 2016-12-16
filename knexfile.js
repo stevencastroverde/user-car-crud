eea6367244f95022b7f94da94032a7734bc1e081
Matt Seaton <matt.r.seaton@gmail.com>
2016-12-16 10:37:25 -0700

    rqd dotenv in knexfile and added production export

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,16 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/carapp'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
-
-
 
 
 };
